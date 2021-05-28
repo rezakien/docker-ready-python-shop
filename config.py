2d4004c2f3f9bc7aa317a612e72172f8e3a82d0b
Rezakien <amirkhanzukhritdinov@gmail.com>
2021-05-28 12:25:29 +0500

    reconstructed config file

--- config.py
+++ config.py
@@ -9,13 +9,19 @@ ADMIN_ID = os.getenv("MY_ID")
 ID_E = os.getenv("ID_E")
 HOST = os.getenv("HOST")
 
+PRODUCTION = True if os.getenv("PRODUCTION") == '1' else False
+
 PG_HOST = os.getenv("PG_HOST")
 PG_USER = os.getenv("PG_USER")
 PG_PASSWORD = os.getenv("PG_PASSWORD")
+PG_CONTAINER_PORT = os.getenv("PG_CONTAINER_PORT")
 PG_HOST_PORT = os.getenv("PG_HOST_PORT")
 DATABASE = os.getenv("DATABASE")
 
-POSTGRES_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_HOST_PORT}/{DATABASE}"
+if PRODUCTION:
+    POSTGRES_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_HOST_PORT}/{DATABASE}"
+else:
+    POSTGRES_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_HOST_PORT}/{DATABASE}"
 
 I18N_DOMAIN = 'shop-bot'
 BASE_DIR = Path(__file__).parent
